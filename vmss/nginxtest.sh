22ae92b4cbee168161f092cf303490eb21109644
Nitin Jain <nitin.solna@gmail.com>
2018-07-20 14:29:45 +0200

    Syntax Fix

--- vmss/nginxtest.sh
+++ vmss/nginxtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-get install -y nginx
-echo "Hello & Welcome to MicroSoft Azure Learning - This request is from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.htm
+echo "Hello & Welcome to MicroSoft Azure Learning - This request is from host - " $HOSTNAME "!" | sudo tee -a /var/www/html/index.htm
