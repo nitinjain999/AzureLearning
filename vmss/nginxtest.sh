cafce8b59789d30921b10f27457dc778dfbcc8ec
Nitin Jain <nitin.solna@gmail.com>
2018-07-21 07:46:39 +0200

    Fix

--- vmss/nginxtest.sh
+++ vmss/nginxtest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-get install -y nginx
-echo "Hello & Welcome to MicroSoft Azure Learning - This request is from host - " $HOSTNAME "!" | sudo tee -a /var/www/html/index.htm
+echo "Hello & Welcome to 70-533 Exam Learning - This request is from host - " $HOSTNAME "!" | sudo tee -a /var/www/html/index.htm
